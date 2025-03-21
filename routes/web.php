a2197a2b36324cfb7eec7acd027e309aa11d6e12
Zharfan23 <zharfanpintar@gmail.com>
2025-03-21 17:03:07 +0700

    add Route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\LoanController;
+use App\Http\Controllers\ReviewController;
 
+// Home route
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('api')->middleware('api')->group(function () {
+    Route::apiResource('users', UserController::class);
+    Route::apiResource('books', BookController::class);
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('loans', LoanController::class);
+    Route::apiResource('reviews', ReviewController::class);
 });
